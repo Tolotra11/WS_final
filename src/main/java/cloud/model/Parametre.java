a965a992e667189bbdf3efcfffa644a8d7430e5f
Tolotra <99733588+Tolotra11@users.noreply.github.com>
2023-02-03 20:15:22 +0300

    update

--- src/main/java/cloud/model/Parametre.java
+++ src/main/java/cloud/model/Parametre.java
@@ -20,19 +20,28 @@ public class Parametre extends ObjectBDD{
     public Double getCommission() {
         return commission;
     }
-    public void setCommission(Double comission) {
+    public void setCommission(Double comission) throws Exception {
+        if(commission < 0 || commission > 100){
+            throw new Exception("Commission invalide");
+        }
         this.commission = comission;
     }
     public Double getDureeEnchereMin() {
         return dureeEnchereMin;
     }
-    public void setDureeEnchereMin(Double dureeEnchereMin) {
+    public void setDureeEnchereMin(Double dureeEnchereMin) throws Exception {
+        if(dureeEnchereMin <= 0){
+            throw new Exception("duree invalide");
+        }
         this.dureeEnchereMin = dureeEnchereMin;
     }
     public Double getDureeEnchereMax() {
         return dureeEnchereMax;
     }
-    public void setDureeEnchereMax(Double dureeEnchereMax) {
+    public void setDureeEnchereMax(Double dureeEnchereMax) throws Exception {
+        if(dureeEnchereMax <= 0){
+            throw new Exception("duree invalide");
+        }
         this.dureeEnchereMax = dureeEnchereMax;
     }
     public void init(){
